9443a8a175da642e53ebb434841f28e42539221b
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2023-04-29 15:15:44 +0700

    Update app/sidekiq/reply_comment_job.rb

--- app/sidekiq/reply_comment_job.rb
+++ app/sidekiq/reply_comment_job.rb
@@ -9,7 +9,7 @@ class ReplyCommentJob
 
     reply_comment = ask_gpt(comment, commentator_name)
 
-    page.put_comment(comment_id, reply_comment)
+    page.put_comment(comment_id, "#{reply_comment} (I'm a Bot. If I make any mistakes, please forgive me. You can find me at www.AllLoveHere.com)")
     page.put_like(comment_id)
 
     Blocker.find_or_create_by(commentator_id: commentator_id)
@@ -28,7 +28,7 @@ class ReplyCommentJob
       }
     )
 
-    input = "Please help me write a creative/engaging comment to reply to this comment #{comment}. The commenter's name is #{commentator_name}. \n- Let's analyze the feelings of this comment first. \n- Depend on the emotion of the comment and reply to them with appreciation, gratitude or an empathetic comment. \n- Be kind/energetic and full of love. \n- And if possible choose an icon for the comment you make, it should match the emotion of the comment, for example the comment's emotion is positive, happy should not choose the sad symbol, and vice versa. \n- Make it 10 to 30 words long. \n- If the message is blank or you can't understand the context, give the person a cute song. \n- Note: My Facebook page represents www.AllLoveHere.com, an online store dedicated to custom jewelry as gifts for various occasions and for a wide audience, mainly between family members and their friends. \n- Place the following text at the end to indicate that you are a GPT bot: \" (I'm a Bot. If I make any mistakes, please forgive me. You can find me at www.AllLoveHere.com)\""
+    input = "Please help me write a creative/engaging comment to reply to this comment #{comment}. The commenter's name is #{commentator_name}. \n- Depend on the emotion of the comment and reply to them with appreciation, gratitude or an empathetic comment. \n- Be kind/energetic and full of love. \n- And if possible choose an icon for the comment you make, it should match the emotion of the comment, for example the comment's emotion is positive, happy should not choose the sad symbol, and vice versa. \n- Make it 10 to 30 words long. \n- If the message is blank or you can't understand the context, give the person a cute song. \n- Note: My Facebook page represents www.AllLoveHere.com, an online store dedicated to custom jewelry as gifts for various occasions and for a wide audience, mainly between family members and their friends."
 
     response = conn.post do |req|
       req.body = {
