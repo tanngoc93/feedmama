50aa8d446ae0ff7102bba5f75f00b618cff8b214
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2023-05-04 15:15:48 +0700

    Big update

--- app/sidekiq/reply_comment_job.rb
+++ app/sidekiq/reply_comment_job.rb
@@ -2,14 +2,14 @@ class ReplyCommentJob
   include Sidekiq::Job
   sidekiq_options retry: 3, dead: false
 
-  GPT_TYPE = ENV.fetch('GPT_TYPE') { 'KOALA' }
+  GPT_TYPE = ENV.fetch('GPT_TYPE') { nil }
 
   def perform(post_id, comment_id, comment, commentator_name, social_account_id)
     social_account = SocialAccount.where(id: social_account_id).first
 
     return unless social_account.present?
 
-    content = ask_openai(comment, commentator_name)
+    content = ask_openai(content = social_account.search_terms, comment, commentator_name)
 
     page = Koala::Facebook::API.new( social_account.resource_access_token )
     page.put_comment(comment_id, "#{content} ( I'm a Bot. If I make any mistakes, please forgive me. You can find me at www.AllLoveHere.com )")
@@ -20,15 +20,16 @@ class ReplyCommentJob
 
   private
 
-  def ask_openai(comment = '', commentator_name = '')
+  def ask_openai(content, comment, commentator_name)
     client = OpenAI::Client.new
 
-    input = "Please help me write a creative/engaging comment to reply to this comment #{comment}. The commenter's name is #{commentator_name}. \n- Depend on the emotion of the comment and reply to them with appreciation, gratitude or an empathetic comment. \n- Be kind/energetic and full of love. \n- And if possible choose an icon for the comment you make, it should match the emotion of the comment, for example the comment's emotion is positive, happy should not choose the sad symbol, and vice versa. \n- Make it 10 to 30 words long. \n- If the message is blank or you can't understand the context, give the person a cute song. \n- Note: My Facebook page represents www.AllLoveHere.com, an online store dedicated to custom jewelry as gifts for various occasions and for a wide audience, mainly between family members and their friends."
+    content = content.sub('#comment', comment)
+    content = content.sub('#fullName', commentator_name)
 
     response = client.chat(
       parameters: {
           model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: input }],
+          messages: [{ role: "user", content: content }],
           temperature: 0.7,
       })
 
