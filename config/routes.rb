83f99813b6ff13ef6d33a72b11d12705fc3dfbd3
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2023-05-05 16:09:10 +0700

    Fix some bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    username == ENV['SIDEKIQ_USER'] && password == ENV['SIDEKIQ_PASSWORD']
-  end
-  mount Sidekiq::Web => "/sidekiq"
   #
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  # 
+  authenticate :admin_user do
+    mount Sidekiq::Web => "/sidekiq"
+  end
   #
   root "homepage#index"
   get  "webhooks/:secured_token", to: "facebooks#subscription"
