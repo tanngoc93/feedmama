4823c50ab3e71057cdfd3fb67b961d3c1545912f
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2023-06-23 16:33:46 +0700

    Update app/admin/auto_comments.rb

--- app/admin/auto_comments.rb
+++ app/admin/auto_comments.rb
@@ -22,8 +22,14 @@ ActiveAdmin.register AutoComment do
   form do |f|
     f.inputs do
       f.input :content
-      f.input :social_account_id
+      f.input :social_account_id, as: :select, collection: SocialAccount.all.collect { |item| [item.resource_name, item.id] }
     end
     f.actions
+  end
+
+  controller do
+    def permitted_params
+      params.permit auto_comment: [:content, :social_account_id]
+    end
   end
 end
