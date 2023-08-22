aa4732b9115e5922d3c666f29e8e718b81ebfe08
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2023-08-22 18:37:21 +0700

    Fx

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js", preload: true
-pin "@nathanvda/cocoon", to: "https://ga.jspm.io/npm:@nathanvda/cocoon@1.2.14/cocoon.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@nathanvda/cocoon", to: "https://ga.jspm.io/npm:@nathanvda/cocoon@1.2.14/cocoon.js"
+
