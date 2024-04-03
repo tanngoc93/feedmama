68f9b059da97c3cd91cc74f7d3b76101e5de5436
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2024-04-03 20:40:00 +0700

    Fix bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = {
+    host: "feedmama.markiee.co",
+    port: 80
+  }
   config.action_mailer.delivery_method = :resend
 
   # Code is not reloaded between requests.
