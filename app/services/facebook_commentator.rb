c1464e47b4beedf85b50d3c38f377988f0ebab06
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2024-04-08 18:05:05 +0700

    Fix bugs

--- app/services/facebook_commentator.rb
+++ app/services/facebook_commentator.rb
@@ -1,21 +1,21 @@
 class FacebookCommentator < ApplicationService
   attr_reader :social_account,
               :comment_id,
-              :message
+              :comment
 
-  def initialize(social_account, comment_id, message)
+  def initialize(social_account, comment_id, comment)
     @social_account = social_account
     @comment_id = comment_id
-    @message = message
+    @comment = comment
   end
 
   def call
-    put_comment(@social_account, @comment_id, @message)
+    put_comment
   end
 
   private
 
-  def put_comment(social_account, comment_id, message)
+  def put_comment
     return unless social_account.present?
 
     conn = Faraday.new(
@@ -25,7 +25,7 @@ class FacebookCommentator < ApplicationService
 
     response = conn.post do |req|
       req.body = {
-        message: message,
+        message: comment,
         access_token: social_account.resource_access_token
       }.to_json
     end
