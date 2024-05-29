ab5879fc2cab560f38e134e2c4901abc3fe62436
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2024-05-29 17:24:53 +0700

    Add pricing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
+  def pricing
+    
+  end
+
   def privacy_policy
     render '/pages/privacy_policy'
   end
