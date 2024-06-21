b0ca2256c7dc90aaef2fd181bff3182fd9ca8285
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2024-06-21 17:07:47 +0700

    Update app/controllers/homepage_controller.rb

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,6 +2,10 @@ class HomepageController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @social_accounts = current_user&.social_accounts.order(:resource_name) || []
+    if current_user
+      @social_accounts = current_user.social_accounts.order(:resource_name)
+    else
+      []
+    end
   end
 end
